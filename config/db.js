01453598f46f0992d206494923655a6d461410b7
shokhruzbek <ashokhruzbek@gmail.com>
2025-03-28 14:09:21 +0500

    db.js is done

--- config/db.js
+++ config/db.js
@@ -1,11 +1,22 @@
+// const { Pool } = require("pg");
+
+// const pool = new Pool({
+//   user: "postgres",
+//   host: "localhost",
+//   database: "navruz",
+//   password: "1111",
+//   port: 1234,
+// });
+
+// module.exports = pool;
+
 const { Pool } = require("pg");
 
 const pool = new Pool({
-  user: "postgres",
-  host: "localhost",
-  database: "navruz",
-  password: "1111",
-  port: 1234,
+  connectionString: 'postgresql://eshmat:i4A1s4mGmmpYtZuCGyqwIgGVvqjOGJZp@dpg-cv4k0r8fnakc73botpjg-a.oregon-postgres.render.com/navruz_hs1n',
+  ssl: {
+    rejectUnauthorized: false, // Required for Render PostgreSQL
+  },
 });
 
 module.exports = pool;
